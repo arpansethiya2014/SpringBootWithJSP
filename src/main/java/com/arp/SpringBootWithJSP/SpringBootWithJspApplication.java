1e25e96425dd7507f7794fec0ff0c9a42e926929
Arpan Sethiya <arpansethiya2014@gmail.com>
2024-05-13 02:25:06 +0530

    changes in main file

--- src/main/java/com/arp/SpringBootWithJSP/SpringBootWithJspApplication.java
+++ src/main/java/com/arp/SpringBootWithJSP/SpringBootWithJspApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootWithJspApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithJspApplication.class, args);
 	}
-
 }
